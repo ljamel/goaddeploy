dc68ef9c87b198a1c57d5ee59311f68440857100
ljamel <djamallamri@yahoo.fr>
2025-01-12 13:55:52 +0100

    Update deploygoad.sh

--- deploygoad.sh
+++ deploygoad.sh
@@ -33,9 +33,9 @@ vagrant plugin install winrm
 vagrant plugin install winrm-elevated
 vagrant plugin install vagrant-vmware-desktop
 
-wget https://releases.hashicorp.com/vagrant-vmware-utility/1.0.4/vagrant-vmware-utility_1.0.4_linux_amd64.zip
+wget https://releases.hashicorp.com/vagrant-vmware-utility/1.0.14/vagrant-vmware-utility_1.0.14_linux_amd64.zip
 sudo mkdir -p /opt/vagrant-vmware-desktop/bin
-sudo unzip -d /opt/vagrant-vmware-desktop/bin vagrant-vmware-utility_1.0.0_linux_amd64.zip
+sudo unzip -d /opt/vagrant-vmware-desktop/bin vagrant-vmware-utility_1.0.14_linux_amd64.zip
 sudo /opt/vagrant-vmware-desktop/bin/vagrant-vmware-utility certificate generate
 sudo /opt/vagrant-vmware-desktop/bin/vagrant-vmware-utility service install
 
