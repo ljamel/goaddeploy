c0845ad3b06a472ac0a08732324dfd1b5f8b9405
ljamel <djamallamri@yahoo.fr>
2025-01-15 21:58:34 +0100

    Update deploygoad.sh

--- deploygoad.sh
+++ deploygoad.sh
@@ -41,6 +41,7 @@ sudo /opt/vagrant-vmware-desktop/bin/vagrant-vmware-utility service install
 sudo vagrant plugin install vagrant-disksize
 vagrant plugin install winrm
 vagrant plugin install winrm-fs
+vagrant plugin install winrm-elevated
 
 # Download GOAD
 if [ ! -d /opt/goad ]; then
