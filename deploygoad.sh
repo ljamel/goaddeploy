c9a112d9d760e77ea8bb86263a483d0a9addd035
ljamel <djamallamri@yahoo.fr>
2025-02-03 20:21:13 +0100

    Update deploygoad.sh

--- deploygoad.sh
+++ deploygoad.sh
@@ -16,6 +16,7 @@ apt-get update
 # Install base packages needed
 apt install git
 apt-get install -y vmware python3-pip pip vagrant
+apt install python3.13-venv
 
 # Enable IP forwarding on Ubuntu
 if [ "`cat /proc/sys/net/ipv4/ip_forward`" != "1" ]; then
